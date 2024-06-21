36ab8e63c486afa887d5fe7321f79126f98fd684
Lenildo <lourencolenildo@gmail.com>
2024-06-21 19:11:43 -0300

    fix: adjustments to LoanController

--- src/main/java/api/simplified/loans/controller/LoanController.java
+++ src/main/java/api/simplified/loans/controller/LoanController.java
@@ -2,6 +2,7 @@ package api.simplified.loans.controller;
 
 import api.simplified.loans.dto.CustomerLoanRequest;
 import api.simplified.loans.dto.CustomerLoanResponse;
+import api.simplified.loans.service.LoanService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,8 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoanController {
+    private final LoanService loanService;
+
+    public LoanController(LoanService loanService) {
+        this.loanService = loanService;
+    }
+
     @PostMapping(value = "/customer-loans")
     public ResponseEntity<CustomerLoanResponse> customerLoans(@RequestBody CustomerLoanRequest loanRequest) {
+      var loanResponse = loanService.checkLoanAvailability(loanRequest);
 
+      return ResponseEntity.ok(loanResponse);
     }
 }
