02946abd7f61c93df7b4f2be5f5bc108fe7d9f91
Lenildo <lourencolenildo@gmail.com>
2024-06-21 18:20:16 -0300

    feat: initial application tests

---
+++ src/test/java/api/simplified/loans/LoansApplicationTests.java
@@ -0,0 +1,13 @@
+package api.simplified.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
