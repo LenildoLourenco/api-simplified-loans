a640694f6550d503ad32cc2d8d6787e80b5ae65e
Lenildo <lourencolenildo@gmail.com>
2024-06-23 13:00:36 -0300

    feat: creation of consigned and guarantee methods

--- src/main/java/api/simplified/loans/domain/Loan.java
+++ src/main/java/api/simplified/loans/domain/Loan.java
@@ -8,12 +8,15 @@ public class Loan {
     }
 
     public boolean isPersonalLoanAvailable() {
-        if (customer.isIncomeEqualOrLowerThan(3000.0)) {
-            return true;
-        }
-      return customer.isIncomeBetween(3000.00, 5000.00)
-              && customer.isAgeLowerThan(30)
-              && customer.isFromLocation("SP");
+        return basicLoanAvailable();
+    }
+
+    public boolean isConsigmentLoanAvailable() {
+        return customer.isIncomeEqualOrGreaterThan(5000.0);
+    }
+
+    public boolean isGuaranteedLoanAvailable() {
+        return basicLoanAvailable();
     }
 
     public double getPersonalLoanInterestRate() {
@@ -21,5 +24,28 @@ public class Loan {
             return 4.0;
         }
         throw new LoanNotAvailableException();
+    }
+
+    public double getGuaranteedLoanInterestRate() {
+        if (isGuaranteedLoanAvailable()) {
+            return 3.0;
+        }
+        throw new LoanNotAvailableException();
+    }
+
+    public double getConsigmentLoanInterestRate() {
+        if (isConsigmentLoanAvailable()) {
+            return 2.0;
+        }
+        throw new LoanNotAvailableException();
+    }
+
+    private boolean basicLoanAvailable() {
+        if (customer.isIncomeEqualOrLowerThan(3000.0)) {
+            return true;
+        }
+        return customer.isIncomeBetween(3000.00, 5000.00)
+                && customer.isAgeLowerThan(30)
+                && customer.isFromLocation("SP");
     }
 }
