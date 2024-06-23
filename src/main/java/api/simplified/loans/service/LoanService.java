a1147298b0ca181a6f58947e6a8510fe7c254028
Lenildo <lourencolenildo@gmail.com>
2024-06-23 13:03:03 -0300

    feat: creation of consignment and guarantee rules

--- src/main/java/api/simplified/loans/service/LoanService.java
+++ src/main/java/api/simplified/loans/service/LoanService.java
@@ -1,12 +1,35 @@
 package api.simplified.loans.service;
 
+import api.simplified.loans.domain.Loan;
+import api.simplified.loans.domain.LoanType;
 import api.simplified.loans.dto.CustomerLoanRequest;
 import api.simplified.loans.dto.CustomerLoanResponse;
+import api.simplified.loans.dto.LoanResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class LoanService {
     public CustomerLoanResponse checkLoanAvailability(CustomerLoanRequest loanRequest) {
-        return null;
+        var customer = loanRequest.toCustomer();
+        var loan = new Loan(customer);
+
+        List<LoanResponse> loans = new ArrayList<>();
+
+        if (loan.isPersonalLoanAvailable()) {
+            loans.add(new LoanResponse(LoanType.PERSONAL, loan.getPersonalLoanInterestRate()));
+        }
+
+        if (loan.isGuaranteedLoanAvailable()) {
+            loans.add(new LoanResponse(LoanType.GUARANTEED, loan.getGuaranteedLoanInterestRate()));
+        }
+
+        if (loan.isConsigmentLoanAvailable()) {
+            loans.add(new LoanResponse(LoanType.CONSIGNMENT, loan.getConsigmentLoanInterestRate()));
+        }
+
+        return new CustomerLoanResponse(loanRequest.name(), loans);
     }
 }
